0d8e3a5f1941a14e726098063052ec573970b0da
Solomon White <rubysolo@gmail.com>
2012-08-02 11:49:26 -0600

    wire up analysis command

--- lib/andrey/command.rb
+++ lib/andrey/command.rb
@@ -9,10 +9,23 @@ module Andrey
       end
     end
 
+    class Analyze
+      def run(*args)
+        probability_map = Andrey::Analyzer.new.read(args.first)
+        puts "["
+        probability_map.each do |row|
+          puts "[#{ row.join(',') }]"
+        end
+        puts "]"
+      end
+    end
+
     def self.[](command)
       case command
       when /^gen/i
         Generate.new
+      when /^ana/i
+        Analyze.new
       else
         raise "unknown command #{ command }"
       end
